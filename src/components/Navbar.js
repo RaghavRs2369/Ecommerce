22405ab849ccdf3185b804ed04caaf9cd09b6cd9
RaghavRs2369 <raghavendran1999@gmail.com>
2022-06-23 10:58:11 +0530

    new commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,114 @@
+import { Badge, Button, IconButton } from '@mui/material';
+import { Search , ShoppingCartOutlined} from '@mui/icons-material';
+import React from 'react'
+import styled from 'styled-components'
+import { useNavigate } from 'react-router-dom';
+
+
+
+
+
+const Container = styled.div`
+         height: 80px;
+          `;
+const Wrapper = styled.div`
+         padding: 10px 20px;
+         display: flex;
+         align-items: center;
+         justify-content: space-between;
+         `;
+const Left = styled.div` 
+          flex: 1;
+          display: flex;
+          align-items: center;
+          `;
+      
+const Center = styled.div`
+flex: 1;
+text-align: center;
+`;
+
+const Logo = styled.h1`
+    font-weight: bold;
+`;
+          
+const Right = styled.div`
+flex: 1;
+display: flex;
+align-items: center;
+justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+     font-size: 14px;
+     cursor: pointer;
+     margin-left: 25px;
+     `
+
+
+const Language = styled.span`
+        font-size: 14px;
+        cursor: pointer;
+        `;
+
+const SearchContainer = styled.div`
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+    `;
+    
+const Input = styled.input`
+   border: none;
+`;   
+
+
+
+const Navbar = () => {
+
+    let navigate = useNavigate()
+
+    const clicked = () => {
+        navigate('/register')
+    }
+  return (
+      <Container>
+        <Wrapper>
+            <Left>
+                <Language>EN</Language>
+                <SearchContainer>
+                    <Input/>
+                    <Search style={{color:"gray",fontSize: 16}}/>
+                </SearchContainer>
+            </Left>
+            <Center><Logo>KNIGHTS.</Logo></Center>
+            <Right>
+                <MenuItem>
+                   <Button variant='contained' color='info' onClick={clicked}>
+                    
+                    register
+                   </Button>
+                </MenuItem>
+                <MenuItem>
+                  <Button href='/signin' variant='contained' color='primary'>
+                     signin
+                  </Button>
+                </MenuItem>
+                <MenuItem>
+               <IconButton href='/cart'>
+               <Badge badgeContent={4} color="primary">
+      <ShoppingCartOutlined  />
+    </Badge>
+               </IconButton>
+                </MenuItem>
+            </Right>
+         
+         </Wrapper>
+      </Container>
+       
+
+  )
+}
+
+export default Navbar
---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://drive.google.com/uc?export=download&id=1TmZax_yE7KLoeTcxaAvgke5NDXJPbxXy",
+        title: "SUMMER SALE",
+        desc: "DON'T  COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://drive.google.com/uc?export=download&id=1TmZax_yE7KLoeTcxaAvgke5NDXJPbxXy",
+        title: "AUTUMN COLLECTION",
+        desc: "DON'T  COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed", 
+    },
+    {
+        id: 3,
+        img: "https://drive.google.com/uc?export=download&id=1TmZax_yE7KLoeTcxaAvgke5NDXJPbxXy",
+        title: "VINTAGE COLLECTION",
+        desc: "DON'T  COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    }
+]
+
+export const categories =[
+    {
+        id: 1,
+        img: "https://drive.google.com/uc?export=download&id=1PVdB2XlZvOmM_SAKGs8j8ZLmsY_iilG9",
+        title: "SHIRT STYLE",
+    },
+    {
+        id: 2,
+        img: "https://drive.google.com/uc?export=download&id=1f4flH4Rq6o_cN-YDP27Sr3_-o4-p4ZIm",
+        title: "SPORTS WEAR",
+    },
+    {
+        id: 3,
+        img: "https://drive.google.com/uc?export=download&id=1RaFXFIjFowe70oiqZvX8jiE8aVbds_Fq",
+        title: "TSHIRT STYLE",
+    },
+];
+
+export const popularProducts = [
+    {
+        id:1,
+        img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+      },
+      {
+        id:2,
+        img:"https://drive.google.com/uc?export=download&id=1NplWBSvHI5IT4sfkUCd55XDN06s7CTef",
+      },
+      {
+        id:3,
+        img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+      },
+      {
+        id:4,
+        img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+      },
+      {
+        id:5,
+        img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+      },
+      {
+        id:6,
+        img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+      },
+      {
+        id:7,
+        img:"https://drive.google.com/uc?export=download&id=14iQL6GluE5N82E8CK9oFfGhr8q4cPb8q",
+      },
+      {
+        id:8,
+        img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    }
+]
